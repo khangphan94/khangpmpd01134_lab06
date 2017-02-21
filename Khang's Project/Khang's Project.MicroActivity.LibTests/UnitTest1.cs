2a7ea95d6b565e7c33d75bf2e85ec42d6cf4f143
Khong He <Khong He@DESKTOP-OD59JV6>
2017-02-21 17:05:56 +0700

    Add project files.

---
+++ Khang's Project/Khang's Project.MicroActivity.LibTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Khang_s_Project.MicroActivity.LibTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
